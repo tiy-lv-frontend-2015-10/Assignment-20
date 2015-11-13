f9a1b32f5819303ca735066103ec5cc3d5ca344f
Thomas Francis Shannon <Thomas@Thomas-MacBook-Pro.local>
2015-11-12 19:31:04 -0800

    started 2nd template

--- main.js
+++ main.js
@@ -2,32 +2,85 @@ $(document).ready(function(){
 
 
 
-	var Meme = Backbone.Model.extend({
+		var Meme = Backbone.Model.extend({
+		  initialize: function () {
+		    console.log("A new meme has been created");
+		  },
+		  _parse_class_name: "Meme",
+
+		});
+
+		var Memes = Backbone.Collection.extend({
+			model:Meme,
+			_parse_class_name:"Meme"
+		});
+
+		var MemeCollection = new Memes();
+
+		MemeCollection.fetch({
+			success: function(resp){
+				var dataObj={'data':resp.toJSON()};
+				var template= $("#memesTemplate").text();
+				var memeHTML= Mustache.render(template,dataObj)
+				$("#memesDiv").html(memeHTML);
+			},
+			error: function(err){
+				console.log(err);
+			}
+		});
+
+
+
+
+
+
+
+
+	var Router = Backbone.Router.extend({
 	  initialize: function () {
-	    console.log("A new meme has been created");
+	    Backbone.history.start({pushState: true});
 	  },
-	  _parse_class_name: "Meme",
-
+	  routes: {
+	    "meme/:objectId":"meme",
+	    "":"index",
+	    
+	  }
 	});
 
-	var Memes = Backbone.Collection.extend({
-		model:Meme,
-		_parse_class_name:"Meme"
+	var router = new Router();
+
+	router.on('route:index', function() {
+		
+
+	})
+
+	router.on('route:meme', function(objectId) {
+	  var meme = new Meme({objectId:objectId});
+	  meme.fetch({
+	  	success: function(resp){
+	  		var memeObj = {'data':resp.toJSON()};
+			var template2=$('#memeTemplate2').text();
+			var personHtml = Mustache.render(template2,memeObj);
+			
+
+	  	}
+	  })
+	  
+
+	  
+	 
 	});
 
-	var MemeCollection = new Memes();
 
-	MemeCollection.fetch({
-		success: function(resp){
-			var dataObj={'data':resp.toJSON()};
-			var template= $("#memeTemplate").text();
-			var memeHTML= Mustache.render(template,dataObj)
-			$("#memesDiv").html(memeHTML);
-		},
-		error: function(err){
-			console.log(err);
-		}
+
+
+	$("body").on('click',"a", function(e){
+	  e.preventDefault();
+	  var href = $(this).attr('href');
+	  href = href.substr(1);
+	  router.navigate(href, {trigger:true});
 	});
+
+
 
 });
-
