a1ea05c6d4a455fc1296bc8200471251c86ac4a3
Jaimarie Velasco <jaimariev@Jaimaries-MacBook-Pro.local>
2015-11-12 18:49:28 -0800

    got images to show and added some style

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
 
+	$("body").on('click', 'a', function(e){
+    e.preventDefault();
+    var href = $(this).attr('href');
+    href = href.substr(1);
+    router.navigate(href,{trigger:true});
+  });
+
 	var Pet = Backbone.Model.extend({
     initialize: function () {
     },
@@ -31,22 +38,46 @@ $(document).ready(function(){
 		}
 	});
 
-	// var Router = Backbone.Router.extend({
-	// 	initialize:function(){
-	// 		Backbone.history.start({pushState: true});
-	// 	},
-	// 	routes:{
-	// 		"user/:objectId": "user",
-	// 		"": "index"
-	// 	}
-	// });
+	var Router = Backbone.Router.extend({
+		initialize:function(){
+			Backbone.history.start({pushState: true});
+		},
+		routes:{
+			"user/:objectId": "user",
+			"add": "add",
+			"edit": "edit",
+			"": "index"
+		}
+	});
 
-	// var route = new Router();
+	var router = new Router();
 
-	// router.on('route:user', function(objectId){
-	// 	var animal = new Pet({objectId: objectId});
-	// 	animal.fetch({
-	// 		var 
-	// 	})
-	// }
+	router.on('route:user', function(objectId){
+		var animal = new Pet({objectId: objectId});
+		animal.fetch({
+			success: function(resp){
+				var petObj = {'pet':resp.toJSON()};
+				var descripTemplate = $("#descripTemplate").text();
+				var petHTML = Mustache.render(descripTemplate, petObj);
+				$("#descripDiv").html(petHTML);
+				$("#imageDiv").hide();
+				$("#descripDiv").show();
+				console.log("success ", resp);
+		}, error: function(err){
+			console.log("error ", err);
+			}
+		})
+	});
+	router.on('route:add', function(){
+		console.log('add page');
+	});
+	router.on('route:edit', function(){
+		console.log('edit page');
+	});
+	router.on('route:index', function(){
+		$("#descripDiv").hide();
+		$("#imageDiv").show();
+		console.log('home page');
+	});
+
 });
