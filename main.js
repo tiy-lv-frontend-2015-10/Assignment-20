74b9f1ef0f4cf8eb4b9232ac7f7d86b3ff584a06
Kristie White <insanelynocturnal@gmail.com>
2015-11-14 21:17:58 -0800

    forms, views, links

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
         defaults: {
             Description: null,
             type: null,
+            title: null,
             url: null
         },
         _parse_class_name: "animal"
@@ -28,33 +29,74 @@ $(document).ready(function() {
         }
     });
     
-    //router
+    //click to add
+    
+    
+    
+    //routing
     var Router = Backbone.Router.extend ({
         initialize:function(){
             Backbone.history.start({pushState:true});
         },
-            routes:{
-                "url/:objectId":"url",
+            routes:{//routes to pages
+                "edit/:objectId":"edit",
+                "add":"add",
+                "detail/:objectId":"detail",
                 "":"index"
-                //"Description/:objectId":"Description",
-                //"type/:objectId":"type"
             }
     });
     
     var router = new Router();
-    router.on('route:url', function(objectId){
+    router.on('route:detail', function(objectId) {
         var animalObj2 = {'mAnimalDescription': AnimalCollections.get(objectId).toJSON()};
         var animalDescriptionTemplate = $("#animalDescriptionTemplate").text();
         var animal2HTML = Mustache.render(animalDescriptionTemplate, animalObj2);
         $("#detailPageDiv").html(animal2HTML);
         $("#picturePageDiv").hide();
+        $("#detailPageDiv").toggleClass();
         $("#detailPageDiv").show();
     });
     
-    router.on('route:index', function(){
+    router.on('route:index', function() {
+        var animalObj3 = {'mAnimalPics': AnimalCollections.get(objectId).toJSON()};
+        var animalDescriptionTemplate = $("#animalDescriptionTemplate").text();
+        var animal3HTML = Mustache.render(animalDescriptionTemplate, animalObj3);
+        $("#picturePageDiv").html(animal3HTML);
         $("#picturePageDiv").show();
         $("#detailPageDiv").hide();
+        $("#addPageDiv").hide();
+        $("#editPageDiv").hide();
     });
+    
+    router.on('route:add', function() {
+        $("#addPageDiv").toggleClass();
+        $("#addPageDiv").show();
+        $("#picturePageDiv").hide();
+        $("#detailPageDiv").hide();
+        $("#editPageDiv").hide();
+    });
+    
+    $("#addButton").on('click', function(e) {
+        e.preventDefault(); 
+        var animalSave = new Animals();
+        animalSave.set({
+            url: $("#saveImg").val(),
+            title: $("#saveTitle").val(),
+            Description: $("#saveDescription").val()
+        })
+    });
+    
+    router.on('route:edit', function(objectId) {
+        var animalObj4 = {'mAnimalEditTemplate': AnimalCollections.get(objectId).toJSON()};
+        var animalEditTemplate = $("#animalEditTemplate").text();
+        var animal4HTML = Mustache.render(animalEditTemplate, animalObj4);
+        $("#editPageDiv").html(animal4HTML);
+        $("#editPageDiv").show();
+        $("#picturePageDiv").hide();
+        $("#detailPageDiv").hide();
+        $("#editPageDiv").hide();
+    });
+    
     
     $("body").on('click', 'a', function(e){
         e.preventDefault();
@@ -63,9 +105,10 @@ $(document).ready(function() {
         router.navigate(href,{trigger:true});
     });
     
+    //var routerDetail = new Router();
+    //router.on('route:')
     
     
-    
-    
+
     
 });//closes document ready
